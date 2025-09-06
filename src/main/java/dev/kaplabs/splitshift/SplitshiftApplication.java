0c162de63a963342e84abb42cf924cec6e2c42a9
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-09-06 17:19:49 -0400

    Initial commit of Spring Boot project

---
+++ src/main/java/dev/kaplabs/splitshift/SplitshiftApplication.java
@@ -0,0 +1,13 @@
+package dev.kaplabs.splitshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kaplabs/splitshift/SplitshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kaplabs.splitshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
